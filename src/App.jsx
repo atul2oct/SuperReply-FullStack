628e15b92ef13a8c2132a67de7c3d80b9c92b346
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2024-12-21 16:49:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import { Button } from "@/components/ui/button"
+const App = () => {
+  return (
+    <div className='h-screen w-screen text-center flex justify-center items-center bg-slate-500'>
+      <Button variant="destructive">Button</Button>
+    </div>
+  )
+}
+
+export default App
